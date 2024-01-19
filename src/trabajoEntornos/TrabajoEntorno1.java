c53dfa2ac359e3a20cced69bfc356c2f041d59f4
jmanuel-mg98 <148326871+jmanuel-mg98@users.noreply.github.com>
2024-01-19 10:23:20 +0100

    completar la clase1

--- src/trabajoEntornos/TrabajoEntorno1.java
+++ src/trabajoEntornos/TrabajoEntorno1.java
@@ -1,10 +1,34 @@
 package trabajoEntornos;
 
+import java.util.Scanner;
+
 public class TrabajoEntorno1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		int contador = 1,num,suma=0,cuenta0=0,sumaMenos=0,totalMenos=0;
+		double media;
+		Scanner sc = new Scanner(System.in);
+			while(contador <= 10) {
+			System.out.print("Introduzca un número:");
+			num = sc.nextInt();
+			if(num > 0) {
+				suma = suma+num;
+			} else if (num < 0) {
+			sumaMenos=sumaMenos+num;
+			totalMenos++;
+			} else {
+				cuenta0++;
+			}
+			contador++;
+		}
+			if (totalMenos!=0) {
+				media=sumaMenos/totalMenos;
+			}
+			media=0;
+			
+		System.out.println("la suma de los positivos es: "+suma);
+		System.out.println("la media de los negativos es: "+media);
+		System.out.println("la cantidad de 0 escrtitos es: "+cuenta0);
 	}
 
-}
+}	
